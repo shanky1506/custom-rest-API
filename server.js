943955574a4387b690b5f7726e77b3d7bb7cc280
sashank-g <sashank.assetmonk@gmail.com>
2020-11-09 19:13:36 +0530

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true, useUnifiedTopology: true})
+
+// ,{ useUnifiedTopology: true } 
+
+const db = mongoose.connection
+db.on('error',(error)=>console.error(error))
+db.once('open',()=>console.log('Connected to Database'))
+
+app.use(express.json())
+
+const subRouter= require('./routes/subscribers')
+app.use('/subs',subRouter)
+
+app.listen(3000,()=>console.log('Server Started running at Port 3000'))
