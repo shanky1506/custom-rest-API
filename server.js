435394f9d93ba87d876196ca30eb26991478343c
shanky1506 <sashank.assetmonk@gmail.com>
2020-11-09 22:58:03 +0530

    changes minor

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ require('dotenv').config()
 const app = express()
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true, useUnifiedTopology: true})
 
-// ,{ useUnifiedTopology: true } 
-
 const db = mongoose.connection
 db.on('error',(error)=>console.error(error))
 db.once('open',()=>console.log('Connected to Database'))
